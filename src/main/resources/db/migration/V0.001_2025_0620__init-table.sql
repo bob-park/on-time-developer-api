a5803ecd44207884693a16fc72f372356cff4805
Bob Park <hwpark11@kakao.com>
2026-08-15 15:21:02 +0900

    feat: ddl 수정

--- src/main/resources/db/migration/V0.001_2025_0620__init-table.sql
+++ src/main/resources/db/migration/V0.001_2025_0620__init-table.sql
@@ -1,3 +1,15 @@
+-- users_authors
+create table users_authors
+(
+    id                 bigserial               not null primary key,
+    user_unique_id     bigint                  not null,
+    author             varchar(100)            not null,
+    created_date       timestamp default now() not null,
+    created_by         varchar(100)            not null,
+    last_modified_date timestamp,
+    last_modified_by   varchar(100)
+);
+
 -- commits
 create table commits
 (
@@ -6,7 +18,9 @@ create table commits
     repo               varchar(100)            not null,
     branch             varchar(100)            not null,
     author             varchar(100)            not null,
+    user_unique_id     bigint                  not null,
     commit_message     text                    not null,
+    commit_date        timestamp               not null,
     created_date       timestamp default now() not null,
     created_by         varchar(100)            not null,
     last_modified_date timestamp,
