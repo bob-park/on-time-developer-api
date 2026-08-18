231b0763c0c9c49babfcca782c6bcdc97075fc2c
Bob Park <hwpark11@kakao.com>
2026-08-19 00:10:09 +0900

    fix: commit exists 와 register 사이에 lock 추가

--- src/main/java/com/malgn/developers/adapter/in/web/api/v1/CommitApiV1.java
+++ src/main/java/com/malgn/developers/adapter/in/web/api/v1/CommitApiV1.java
@@ -28,29 +28,37 @@ import com.malgn.developers.application.provided.model.CommitExistsResult;
 import com.malgn.developers.application.provided.model.CommitQueryCriteria;
 import com.malgn.developers.application.provided.model.CommitRegisterCommand;
 import com.malgn.developers.application.provided.model.CommitResult;
+import com.malgn.starter.lock.RedisNamedLockProvider;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "developers/commits", version = "1")
 public class CommitApiV1 {
 
+    private static final String LOCK_KEY_COMMIT_ID = "commit:lock:%s";
+
     private final CommitQuery commitQuery;
     private final CommitRegister commitRegister;
+
+    private final RedisNamedLockProvider lockProvider;
 
     @PostMapping(path = "")
     public CommitResponseV1 register(@RequestBody @Valid CommitRegisterRequestV1 request) {
 
         CommitResult result =
-            commitRegister.register(
-                CommitRegisterCommand.builder()
-                    .commitId(request.commitId())
-                    .repo(request.repo())
-                    .branch(request.branch())
-                    .author(request.author())
-                    .userUniqueId(request.userUniqueId())
-                    .commitMessage(request.commitMessage())
-                    .commitDate(request.commitDate())
-                    .build());
+            lockProvider.getLock(
+                LOCK_KEY_COMMIT_ID.formatted(
+                    request.commitId()),
+                () -> commitRegister.register(
+                    CommitRegisterCommand.builder()
+                        .commitId(request.commitId())
+                        .repo(request.repo())
+                        .branch(request.branch())
+                        .author(request.author())
+                        .userUniqueId(request.userUniqueId())
+                        .commitMessage(request.commitMessage())
+                        .commitDate(request.commitDate())
+                        .build()));
 
         return from(result);
 
@@ -78,7 +86,8 @@ public class CommitApiV1 {
 
     @GetMapping(path = "/exists/{commitId}")
     public CommitExistsResponseV1 exists(@PathVariable String commitId) {
-        CommitExistsResult result = commitQuery.exists(commitId);
+        CommitExistsResult result =
+            lockProvider.getLock(LOCK_KEY_COMMIT_ID.formatted(commitId), () -> commitQuery.exists(commitId));
 
         return CommitExistsResponseV1.from(result);
     }
